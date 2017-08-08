fe1c445fab57803a4b7373c4cffba956d24ad551
rationalcoder <rationalcoder@gmail.com>
2017-08-07 19:47:51 -0500

    Minor changes

--- tests/basic.cpp
+++ tests/basic.cpp
@@ -4,23 +4,39 @@
 int main()
 {
     fmt::TreeSettings settings;
-    settings.bar = '|';
-    settings.elbow = '+';
-    settings.shift = 4;
+    settings.shift = 3;
 
     fmt::Tree tree(settings);
 
-    tree.printf_root("Root\n");
+    tree.printf_root("Test\n");
+    tree.printf_level(1, "Test\n");
+    tree.printf_level(2, "Test\n");
+    tree.printf_level(3, "Test\n");
+    tree.printf_level(3, "Test\n");
+    tree.printf_last(3, "Test\n");
+    tree.printf_last(2, "Test\n");
+
     tree.printf_level(1, "Test\n");
     tree.printf_level(2, "Test\n");
     tree.printf_last(2, "Test\n");
-    tree.printf_level(1, "Test\n");
-    tree.printf_level(2, "Test\n");
-    tree.printf_last(2, "Test\n");
+
     tree.printf_last(1, "Test\n");
     tree.printf_level(2, "Test\n");
     tree.printf_level(2, "Test\n");
+    tree.printf_level(3, "Test\n");
+    tree.printf_level(3, "Test\n");
+    tree.printf_last(3, "Test\n");
+    tree.printf_level(4, "Test\n");
+    tree.printf_level(4, "Test\n");
+    tree.printf_last(4, "Test\n");
+
     tree.printf_last(2, "Test\n");
+    tree.printf_level(3, "Test\n");
+    tree.printf_level(3, "Test\n");
+    tree.printf_last(3, "Test\n");
+    tree.printf_level(4, "Test\n");
+    tree.printf_level(4, "Test\n");
+    tree.printf_last(4, "Test\n");
 
     return EXIT_SUCCESS;
 }
