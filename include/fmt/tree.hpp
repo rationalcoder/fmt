035b7401086b6a2ea8d3a90909a14dea0eb30c22
Blake Martin <rationalcoder@gmail.com>
2017-08-09 15:21:30 -0500

    Added some convenience functions and made root printing functions automatically reset the tree.

--- include/fmt/tree.hpp
+++ include/fmt/tree.hpp
@@ -27,18 +27,22 @@ public:
     void printf_root(const char* fmt, ...);
     void printf_level(uint32_t level, const char* fmt, ...);
     void printf_last(uint32_t level, const char* fmt, ...);
+    void printf_last_if(bool cond, uint32_t level, const char* fmt, ...);
 
     void fprintf_root(FILE* file, const char* fmt, ...);
     void fprintf_level(FILE* file, uint32_t level, const char* fmt, ...);
     void fprintf_last(FILE* file, uint32_t level, const char* fmt, ...);
+    void fprintf_last_if(bool cond, FILE* file, uint32_t level, const char* fmt, ...);
 
     void vprintf_root(const char* fmt, va_list list);
     void vprintf_level(uint32_t level, const char* fmt, va_list list);
     void vprintf_last(uint32_t level, const char* fmt, va_list list);
+    void vprintf_last_if(bool cond, uint32_t level, const char* fmt, va_list list);
 
     void vfprintf_root(FILE* file, const char* fmt, va_list list);
     void vfprintf_level(FILE* file, uint32_t level, const char* fmt, va_list list);
     void vfprintf_last(FILE* file, uint32_t level, const char* fmt, va_list list);
+    void vfprintf_last_if(bool cond, FILE* file, uint32_t level, const char* fmt, va_list list);
 
     TreeSettings settings() const { return settings_; }
     void set_settings(const TreeSettings& settings) { settings_ = settings; }
@@ -62,6 +66,7 @@ private:
 
 inline void Tree::printf_root(const char* fmt, ...)
 {
+    reset();
     va_list argList;
     va_start(argList, fmt);
     vfprintf_root(stdout, fmt, argList);
@@ -84,8 +89,18 @@ inline void Tree::printf_last(uint32_t level, const char* fmt, ...)
     va_end(argList);
 }
 
+inline void Tree::printf_last_if(bool cond, uint32_t level, const char* fmt, ...)
+{
+    va_list argList;
+    va_start(argList, fmt);
+    if (cond) vfprintf_last(stdout, level, fmt, argList);
+    else vfprintf_level(stdout, level, fmt, argList);
+    va_end(argList);
+}
+
 inline void Tree::fprintf_root(FILE* file, const char* fmt, ...)
 {
+    reset();
     va_list argList;
     va_start(argList, fmt);
     vfprintf_root(file, fmt, argList);
@@ -108,8 +123,18 @@ inline void Tree::fprintf_last(FILE* file, uint32_t level, const char* fmt, ...)
     va_end(argList);
 }
 
+inline void Tree::fprintf_last_if(bool cond, FILE* file, uint32_t level, const char* fmt, ...)
+{
+    va_list argList;
+    va_start(argList, fmt);
+    if (cond) vfprintf_last(file, level, fmt, argList);
+    else vfprintf_level(file, level, fmt, argList);
+    va_end(argList);
+}
+
 inline void Tree::vprintf_root(const char* fmt, va_list list)
 {
+    reset();
     vprintf(fmt, list);
 }
 
@@ -123,8 +148,17 @@ inline void Tree::vprintf_last(uint32_t level, const char* fmt, va_list list)
     vfprintf_last(stdout, level, fmt, list);
 }
 
+inline void Tree::vprintf_last_if(bool cond, uint32_t level, const char* fmt, va_list list)
+{
+    if (cond)
+        vfprintf_last(stdout, level, fmt, list);
+    else
+        vfprintf_level(stdout, level, fmt, list);
+}
+
 inline void Tree::vfprintf_root(FILE* file, const char* fmt, va_list list)
 {
+    reset();
     vfprintf(file, fmt, list);
 }
 
@@ -191,6 +225,14 @@ inline void Tree::vfprintf_last(FILE* file, uint32_t level, const char* fmt, va_
     vfprintf(file, fullFormat, list);
 
     levelTerminationState_[level-1] = true;
+}
+
+inline void Tree::vfprintf_last_if(bool cond, FILE* file, uint32_t level, const char* fmt, va_list list)
+{
+    if (cond)
+        vfprintf_last(file, level, fmt, list);
+    else
+        vfprintf_level(file, level, fmt, list);
 }
 
 inline void Tree::resize_level_state(uint32_t level)
